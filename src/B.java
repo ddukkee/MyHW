fb7d791b0b13f8cd0f270d7a370996e82856e193
ddukkee <ddukkee@naver.com>
2018-06-15 20:44:44 +0900

    first ver

---
+++ src/B.java
@@ -0,0 +1,12 @@
+public class B {
+	int num;
+	public B(){
+	this.num=0;
+	}
+	public B(int num){
+	this.num=num;
+	}
+	public void print(){
+	System.out.println(num);
+	}
+}
---
+++ src/C.java
@@ -0,0 +1,12 @@
+public class C{
+	String str;
+	public C(){
+	this.str="";
+	}
+	public C(String str){
+	this.str=str;
+	}
+	public void print(){
+	System.out.println(str);
+	}
+}
